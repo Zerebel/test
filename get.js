a9bf5b11f5cb2150b12f82199fe713294e2f641e
Zerebel Tanu <zerebeltanu@gmail.com>
2023-08-24 14:52:09 -0500

    start

---
+++ get.js
@@ -0,0 +1,20 @@
+const url = "http://localhost:8000/test";
+const data = {
+  msg: "testing",
+};
+const headers = {
+  "Content-Type": "application/json",
+};
+
+fetch(url, {
+  method: "POST",
+  headers: headers,
+  body: JSON.stringify(data),
+})
+  .then((response) => response.json())
+  .then((data) => {
+    console.log("Response:", data);
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
